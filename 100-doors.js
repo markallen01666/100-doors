0bff18a216d7372737e9ebfd8ce078ec615e03c1
Mark Allen <markallen.webdeveloper@gmail.com>
2019-10-14 15:12:44 +0100

    Solution for FCC/Rosetta Code 100 doors problem - tested on FCC

--- 100-doors.js
+++ 100-doors.js
@@ -1,21 +1,21 @@
 // Solution to FCC/Rosetta Code problem '100 Doors'
 
-function getFinalOpenedDoors (numDoors) {
-  const doors = new Array(numDoors)
-  doors.fill(false, 0, numDoors) // true == 'open', false == 'closed'
+function getFinalOpenedDoors(numDoors) {
+  const doors = new Array(numDoors);
+  doors.fill(false, 0, numDoors); // true == 'open', false == 'closed'
   for (let passNum = 0; passNum < numDoors; passNum++) {
-    const stepDistance = passNum + 1 // number of doors to jump past in a pass
+    const stepDistance = passNum + 1; // number of doors to jump past
     for (let step = passNum; step < numDoors; step += stepDistance) {
-      doors[step] = !doors[step] // toggle door status
+      doors[step] = !doors[step]; // toggle door status
     }
   }
-  const openDoors = [] // array with positions of open doors
+  const openDoors = []; 
   doors.forEach((door, index) => {
     if (door === true) {
-      openDoors.push(index + 1)
+      openDoors.push(index + 1);
     }
-  })
-  return openDoors
+  });
+  return openDoors;
 }
 
-getFinalOpenedDoors(100)
+getFinalOpenedDoors(100);
